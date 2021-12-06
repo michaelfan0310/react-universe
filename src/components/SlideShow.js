14d26b11527c5b9d1541ee65b70498cb3dcf075b
michaelfan0310 <michaelfan0310@gmail.com>
2021-12-05 20:38:59 -0500

    w-60

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -2,10 +2,10 @@ import Carousel from 'react-bootstrap/Carousel';
 import './SlideShow.css';
 
 function SlideShow(){
-  const slides= new Array(3);
-      slides[0]="images/cover2.jpg";
-      slides[1]="images/cover4.jpg";
-      slides[2]="images/cover5.jpg";
+  // const slides= new Array(3);
+  //     slides[0]="images/cover2.jpg";
+  //     slides[1]="images/cover4.jpg";
+  //     slides[2]="images/cover5.jpg";
   // const slidesItem=slides.map(slides[id].scale*1.2);
       
      return (<div>
@@ -13,10 +13,10 @@ function SlideShow(){
   
   <Carousel.Item interval={2500}>
     <img
-      className="d-block-center w-40"     
+      className="d-block-center w-60"     
       src="images/cover11.jpg"
       alt="cover"
-      width="390"
+      
       height="290"      
     />
     {/* <Carousel.Caption>
@@ -29,20 +29,20 @@ function SlideShow(){
 
  <Carousel.Item interval={2000}>
     <img
-      className="d-block-center w-40"
+      className="d-block-center w-60"
       src="images/cover6.png"
       alt=""
-      width="390"
+      
       height="290"    
     />  
   </Carousel.Item>
 
   <Carousel.Item interval={1500}>
     <img
-      className="d-block-center w-40"
+      className="d-block-center w-60"
       src="images/cover5.jpg"
       alt="Third slide"
-      width="390" 
+       
       height="290" 
           
     />   
@@ -50,28 +50,28 @@ function SlideShow(){
 
   <Carousel.Item interval={1000}>
     <img
-      className="d-block-center w-40"
+      className="d-block-center w-60"
       src="images/cover7.jpg"
       alt=""
-      width="390"
+      
       height="290"      
     />   
   </Carousel.Item>
   <Carousel.Item interval={500}>
     <img
-      className="d-block-center w-40"
+      className="d-block-center w-60"
       src="images/cover15.jpg"
       alt=""
-      width="390"
+      
       height="290"    
     />  
   </Carousel.Item>
    <Carousel.Item >
     <img
-      className="d-block-center w-40"
+      className="d-block-center w-60"
       src="images/cover16.jpg"
       alt=""
-      width="390"
+      
       height="290"    
     />  
   </Carousel.Item>
